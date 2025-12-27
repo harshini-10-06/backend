56fd770a4a7bd7c921169a4a450a7cf28dea89af
harshini-10-06 <s.g.harshini2006@gmail.com>
2025-12-27 12:58:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,22 @@
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const courseRoutes = require("./routes/courses");   // Courses API
-const authRoutes = require("./routes/auth");        // Auth API
+const courseRoutes = require("./routes/courses");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Connect MongoDB
-mongoose.connect("YOUR_MONGODB_URL")
-    .then(() => console.log("Database connected"))
-    .catch(err => console.log(err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("Database connected"));
 
-// Routes
 app.use("/courses", courseRoutes);
 app.use("/auth", authRoutes);
 
-// Test root
-app.get("/", (req, res) => {
-    res.send("Mini LMS Backend Running");
-});
+app.get("/", (req, res) => res.send("Mini LMS Backend Running"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
