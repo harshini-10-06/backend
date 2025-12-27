a1a2124085114aace05f5d48323653af1d7370ed
harshini-10-06 <s.g.harshini2006@gmail.com>
2025-12-27 12:56:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const courseRoutes = require("./routes/courses");   // Courses API
+const authRoutes = require("./routes/auth");        // Auth API
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://username:password@cluster.mongodb.net/minilms"
-).then(() => console.log("DB Connected"));
+// Connect MongoDB
+mongoose.connect("YOUR_MONGODB_URL")
+    .then(() => console.log("Database connected"))
+    .catch(err => console.log(err));
 
+// Routes
+app.use("/courses", courseRoutes);
+app.use("/auth", authRoutes);
+
+// Test root
 app.get("/", (req, res) => {
-  res.send("Mini LMS Backend Running");
+    res.send("Mini LMS Backend Running");
 });
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
